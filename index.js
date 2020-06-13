b8d7d8a6361e6201769fffa2edf9758f49f1001e
sor-alan <alanpfabricio@gmail.com>
2020-06-13 15:15:38 -0300

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bot = new TelegramBot( TOKEN, { polling: true });
 bot.on('message', (msg) => {
     bot.sendMessage(chatId, 'Received your message');
     
-    // bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem-vindo ao BoloMaster! Na verdade, bem-vindo é o caralho. Quer educação volte para o Whatsapp. Esse grupo aqui só tem gente da pior ladainha. Tem biscoiteira, tem LGBT, tem negro bandido, maconheiro, tem vagabundo, tem machista. Só gente com tendência suicida, tá entendendo? Salve-se enquanto ainda pode. SAIA DAQUI URGENTE.`)
+    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem-vindo ao BoloMaster! Na verdade, bem-vindo é o caralho. Quer educação volte para o Whatsapp. Esse grupo aqui só tem gente da pior ladainha. Tem biscoiteira, tem LGBT, tem negro bandido, maconheiro, tem vagabundo, tem machista. Só gente com tendência suicida, tá entendendo? Salve-se enquanto ainda pode. SAIA DAQUI URGENTE.`)
 })
 
 // bot.on("polling_errorbot.on", ("polling_error", (err) => console.log(err)));
