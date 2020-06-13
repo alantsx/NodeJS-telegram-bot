03d741908284c0187a503cb56e5015cdbcc57a07
sor-alan <alanpfabricio@gmail.com>
2020-06-12 22:41:50 -0300

    Bolo Bot

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '795725413:AAGy80ZQKV9Z2s5rs7noy6dJAH4Or68X39o';
+
+const bot = new TelegramBot( TOKEN, { polling: true });
+
+bot.on('new_chat_members', (msg) => {
+    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem-vindo ao BoloMaster! Na verdade, bem-vindo é o caralho. Quer educação volte para o Whatsapp. Esse grupo aqui só tem gente da pior ladainha. Tem biscoitera, tem LGBT, tem negro bandido, maconheiro, tem vagabundo, tem machista. Só gente com tendência suicida, tá entendendo? Salve-se enquanto ainda pode. SAIA DAQUI URGENTE.`)
+})
