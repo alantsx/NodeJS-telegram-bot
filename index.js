fb24de8aace4baf9e38402e75a8cba73fab61a83
sor-alan <alanpfabricio@gmail.com>
2020-06-13 15:35:40 -0300

    fix

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 process.env.NTBA_FIX_319 = 1;
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '795725413:AAGy80ZQKV9Z2s5rs7noy6dJAH4Or68X39o';
