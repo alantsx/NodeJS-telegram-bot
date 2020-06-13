d20ef63a82a5560301d0183fd21df6bcb491befb
sor-alan <alanpfabricio@gmail.com>
2020-06-13 17:26:31 -0300

    Update v0.011

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 process.env.NTBA_FIX_319 = 1;
+var express = require('express');
+var app     = express();
 
-process.env.PORT = 3000;
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const TelegramBot = require('node-telegram-bot-api');
 
