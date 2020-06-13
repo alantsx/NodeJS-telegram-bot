7b558f532b9aa5e732bf98a45f378b0a18dbd752
sor-alan <alanpfabricio@gmail.com>
2020-06-13 15:20:42 -0300

    fix

--- index.js
+++ index.js
@@ -11,5 +11,4 @@ bot.on('message', (msg) => {
     
     bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem-vindo ao BoloMaster! Na verdade, bem-vindo é o caralho. Quer educação volte para o Whatsapp. Esse grupo aqui só tem gente da pior ladainha. Tem biscoiteira, tem LGBT, tem negro bandido, maconheiro, tem vagabundo, tem machista. Só gente com tendência suicida, tá entendendo? Salve-se enquanto ainda pode. SAIA DAQUI URGENTE.`)
 })
-
-// bot.on("polling_errorbot.on", ("polling_error", (err) => console.log(err)));
+bot.on("polling_error", console.log);
